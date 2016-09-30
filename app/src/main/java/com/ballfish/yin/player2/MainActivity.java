1ade39eeac31b22bd72c8d8d8549a759f542c5a2
Tzu-Yin Hong <lili668668@gmail.com>
2016-09-30 17:43:41 +0800

    Set Player Adapter

--- app/src/main/java/com/ballfish/yin/player2/MainActivity.java
+++ app/src/main/java/com/ballfish/yin/player2/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -11,10 +13,21 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView playerListRecycle;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerListRecycle = (RecyclerView) findViewById(R.id.player_list_recycle);
+        playerListRecycle.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        playerListRecycle.setLayoutManager(layoutManager);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
---
+++ app/src/main/java/com/ballfish/yin/player2/PlayerListAdapter.java
@@ -0,0 +1,29 @@
+package com.ballfish.yin.player2;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PlayerListAdapter extends RecyclerView.Adapter<PlayerListAdapter.ViewHolder> {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    @Override
+    public PlayerListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
